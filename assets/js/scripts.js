af029d4fd7ae24cd20cf8a7fdc8a59db56a4e05c
crazyyy <crazyyy@gmail.com>
2016-04-13 10:10:24 +0300

    prerelise

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -15,10 +15,61 @@
     }
 }
 }());
-if (typeof jQuery == 'undefined') {
-  console.warn('jQuery hasn\'t loaded');
-} else {
-  console.log('jQuery has loaded');
-}
-// Place any jQuery/helper plugins in here.
 
+var request = new XMLHttpRequest();
+request.onload = function() {
+  var logins = '';
+  var needLogins = 10;
+
+  // get the file contents
+  var fileContent = this.responseText;
+  // split into lines
+  var fileContentLines = fileContent.split( '\n' );
+
+  for ( i=0; i <= needLogins; i++) {
+    // get a random index (line number)
+    var randomLineIndex = Math.floor( Math.random() * fileContentLines.length );
+    // extract the value
+    var randomLine = fileContentLines[ randomLineIndex ];
+    randomLine = randomLine.replace(/\s/gi, '');
+    randomLine = randomLine.replace(/\d\d\d/gi, '');
+    var addNubmer = parseInt(randomBetween(10,99));
+    if (randomLine.length < 7 ){
+      randomLine = randomLine + addNubmer;
+      randomLine = randomLine.replace(/\d\d\d/gi, '');
+
+
+    } else if (  randomLine.match(/\d+/g)  )  {
+      randomLine = randomLine + addNubmer;
+      randomLine = randomLine.replace(/\d\d\d/gi, '');
+
+    } else {
+      randomLine = randomLine;
+      randomLine = randomLine + addNubmer;
+      randomLine = randomLine.replace(/\d\d\d/gi, '');
+
+    }
+
+    if (randomLine.length < 7 ) {
+      i = i - 1;
+    } else {
+      logins = logins + '3: ' + randomLine + '<br />';
+    }
+  }
+
+
+
+  // add the random line in a div
+  document.getElementById( 'random-nicknames' ).innerHTML = logins;
+};
+request.open( 'GET', 'sorces.txt', true );
+request.send();
+
+
+function randomBetween(min, max) {
+  if (min < 0) {
+    return min + Math.random() * (Math.abs(min)+max);
+  } else {
+    return min + Math.random() * max;
+  }
+}
